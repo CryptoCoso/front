fe3686cae26782278da4c5fb6c0b5b8444fd4825
BryanOx <bryanoxley1999@gmail.com>
2022-05-22 21:36:06 -0300

    Navbar improvement and ipfs sdk isntallation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import { useContext, useState } from "react";
-import { ethers } from "ethers";
-import { Erc20__factory } from "../contracts/types";
-import { Maybe } from "@metamask/providers/dist/utils";
-import { Button } from "@mui/material";
+import ipfs from 'ipfs'
 
 const Index = () => {
   const { state: appState, setState: setAppState } = useContext(AppContext);
@@ -10,68 +7,23 @@ const Index = () => {
   const [tokenBalance, setTokenBalance] = useState<string>();
   const [tokenAddress, setTokenAddress] = useState<string>();
   const { provider, account } = appState;
+  const [meta, setMeta] = useState<string>("No metadata yet");
 
-  const connect = async () => {
-    if (!window.ethereum?.request) {
-      alert("MetaMask is not installed!");
-      return;
-    }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const provider = new ethers.providers.Web3Provider(window.ethereum as any);
-    const accounts: Maybe<string[]> = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-
-    if (!accounts || !accounts[0]) {
-      alert("MetaMask is not installed!");
-      return;
-    }
-    console.log(accounts[0], provider);
-
-    setAppState({
-      account: accounts[0],
-      provider,
-    });
-    console.log("saved stuff", account, provider);
-  };
-
-  const getTokenBalance = async () => {
-    if (!provider || !account) {
-      alert("Error: Account not connected!");
-      return;
-    }
-    if (!tokenAddress) {
-      alert("Error: Token address is empty!");
-      return;
-    }
-
-    // This might not exist, so we need to use ´npm typechain´ to generate it
-    const token = Erc20__factory.connect(tokenAddress, provider.getSigner());
-
-    const rawBalance = await token.balanceOf(account);
-    const decimals = await token.decimals();
-
-    const balance = ethers.utils.formatUnits(rawBalance, decimals);
-    setTokenBalance(balance);
+  const getMeta = async () => {
+    // const IPFS = ipfs.()
+    // const meta = ipfs.urlSource('QmcXuh9t39hQjvQeDM9zByUaRQRVEtUw47pLnt7NUjcRib');
+    // setMeta(JSON.stringify(meta));
   };
 
   return (
     <>
-      <Button
-        variant="outlined"
-        onClick={connect}
-        disabled={account ? true : false}
-      >
-        Connect
-      </Button>
-      <Button variant="contained" onClick={getTokenBalance}>
-        Get Token Balance
-      </Button>
       <p>Account: {account}</p>
-      <button onClick={getTokenBalance}>Get Token Balance</button>
+      {/* <button onClick={getTokenBalance}>Get Token Balance</button> */}
       <p>Token Balance: {tokenBalance}</p>
       <p>Token address:</p>
       <input type="text" onChange={(e) => setTokenAddress(e.target.value)} />
+      <button onClick={getMeta}>Get Metadata</button>
+      <code lang="json">{meta}</code>
     </>
   );
 };
