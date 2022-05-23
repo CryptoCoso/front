f8f114910bf16c5aec8490b263d9bd0a08c2097f
BryanOx <bryanoxley1999@gmail.com>
2022-05-22 21:36:06 -0300

    Navbar improvement and ipfs sdk isntallation

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,11 @@
+import { Box } from "@mui/material";
 import { Navbar } from "./navbar";
 
 export const Layout: React.FC = ({ children }) => {
   return (
-    <>
+    <Box flexGrow={1} display="flex" flexDirection="column">
       <Navbar />
       <main>{children}</main>
-    </>
+    </Box>
   );
 };
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,7 @@
 import {
   Avatar,
   Box,
+  Button,
   Divider,
   IconButton,
   ListItemIcon,
@@ -9,10 +10,16 @@ import {
   Tooltip,
   Typography,
 } from "@mui/material";
+import { ethers } from "ethers";
 import { Settings, Logout, PersonAdd } from "@mui/icons-material";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AppContext } from "../context/state";
+import { Maybe } from "@metamask/providers/dist/utils";
+import { Erc20__factory } from "../contracts/types";
 
 export const Navbar = () => {
+  const { state: appState, setState: setAppState } = useContext(AppContext);
+
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -21,11 +28,78 @@ export const Navbar = () => {
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const [tokenBalance, setTokenBalance] = useState<string>();
+  const [tokenAddress, setTokenAddress] = useState<string>();
+  const { provider, account } = appState;
+
+  const connect = async () => {
+    if (!window.ethereum?.request) {
+      alert("MetaMask is not installed!");
+      return;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+    const accounts: Maybe<string[]> = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    if (!accounts || !accounts[0]) {
+      alert("MetaMask is not installed!");
+      return;
+    }
+    console.log(accounts[0], provider);
+
+    setAppState({
+      account: accounts[0],
+      provider,
+    });
+    console.log("saved stuff", account, provider);
+  };
+
+  const getTokenBalance = async () => {
+    if (!provider || !account) {
+      alert("Error: Account not connected!");
+      return;
+    }
+    if (!tokenAddress) {
+      alert("Error: Token address is empty!");
+      return;
+    }
+
+    // This might not exist, so we need to use ´npm typechain´ to generate it
+    const token = Erc20__factory.connect(tokenAddress, provider.getSigner());
+
+    const rawBalance = await token.balanceOf(account);
+    const decimals = await token.decimals();
+
+    const balance = ethers.utils.formatUnits(rawBalance, decimals);
+    setTokenBalance(balance);
+  };
+
   return (
-    <>
+    <Box display="flex" flexGrow={1} justifyContent="space-between" width="100vw">
+
       <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
         <Typography sx={{ minWidth: 100 }}>Contact</Typography>
         <Typography sx={{ minWidth: 100 }}>Profile</Typography>
+      </Box>
+
+      <Box>
+        {!account ? (
+          <Button
+            variant="outlined"
+            onClick={connect}
+            disabled={account ? true : false}
+          >
+            Connect
+          </Button>
+        ) : (
+          <Button variant="contained" onClick={getTokenBalance}>
+            Get Token Balance
+          </Button>
+        )}
+
         <Tooltip title="Account settings">
           <IconButton
             onClick={handleClick}
@@ -39,6 +113,7 @@ export const Navbar = () => {
           </IconButton>
         </Tooltip>
       </Box>
+
       <Menu
         anchorEl={anchorEl}
         id="account-menu"
@@ -100,6 +175,7 @@ export const Navbar = () => {
           Logout
         </MenuItem>
       </Menu>
-    </>
+
+    </Box>
   );
 };
