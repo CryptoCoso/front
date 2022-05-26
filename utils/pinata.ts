c5035f67fbdc878454480da0dcbe244fb103f271
Bryan0x <boxley@xseed.com.uy>
2022-05-26 10:26:40 -0300

    Empezamos a conectar front y back y nos bloqueo CORS xd

---
+++ utils/pinata.ts
@@ -0,0 +1,61 @@
+const baseUri = 'nfrenting.herokuapp.com';
+
+const testAuth = async (): Promise<unknown> => {
+  const response = await fetch(`https://${baseUri}/auth`, {
+    method: 'GET',
+  });
+  return response.json();
+};
+
+const getImages = async (): Promise<unknown> => {
+  try {
+    const response = await fetch(`https://${baseUri}/get-images`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Couldn't get images",
+    };
+  }
+}
+
+const getMetadata = async (): Promise<unknown> => {
+  try {
+    const response = await fetch(`https://${baseUri}/get-metadata`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Couldn't get metadata",
+    };
+  }
+}
+
+const mintNft = async (formData: FormData): Promise<unknown> => {
+  try {
+    const response = await fetch(`https://${baseUri}/mint-nft`, {
+      method: 'POST',
+      body: formData,
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Couldn't mint NFT",
+    };
+  }
+}
+
+export default {
+  testAuth,
+  getImages,
+  getMetadata,
+  mintNft,
+}
