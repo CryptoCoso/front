0258cf2b35c0fa16f90a08b803ca97730c349b39
BryanOx <bryanoxley1999@gmail.com>
2022-05-28 19:08:02 -0300

    Error 500 returned if an error occurs on the server

--- utils/pinata.ts
+++ utils/pinata.ts
@@ -58,6 +58,7 @@ const uploadNFTData = async (formData: FormData): Promise<NFTCreationResponse> =
     console.error(error);
     return {
       message: "Couldn't mint NFT",
+      code: 500,
     };
   }
 }
